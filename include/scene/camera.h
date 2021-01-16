5c7fb5172d2a1a8a5d64db85961e1b4ffb572e1f
Polina Ozhiganova <44429468+DoktaPola@users.noreply.github.com>
2021-01-17 01:29:01 +0300

    hot fix

--- include/scene/camera.h
+++ include/scene/camera.h
@@ -1,4 +1,4 @@
-#pragma once 
+#pragma once
 
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
@@ -7,47 +7,47 @@
 #include <glm/gtc/matrix_transform.hpp>
 
 class Camera {
- private:
-	float fov;
-	float znear, zfar;
+private:
+    float fov;
+    float znear, zfar;
 
-	void updateViewMatrix();
- public:
-	enum CameraType { lookat, firstperson };
-	CameraType type = CameraType::lookat;
+    void updateViewMatrix();
+public:
+    enum CameraType { lookat, firstperson };
+    CameraType type = CameraType::lookat;
 
-	glm::vec3 rotation = glm::vec3();
-	glm::vec3 position = glm::vec3();
+    glm::float3 rotation = glm::float3();
+    glm::float3 position = glm::float3();
 
-	float rotationSpeed = 1.0f;
-	float movementSpeed = 1.0f;
+    float rotationSpeed = 1.0f;
+    float movementSpeed = 1.0f;
 
-	bool updated = false;
+    bool updated = false;
 
-	struct
-	{
-		glm::mat4 perspective;
-		glm::mat4 view;
-	} matrices;
+    struct
+    {
+        glm::float4x4 perspective;
+        glm::float4x4 view;
+    } matrices;
 
-	struct
-	{
-		bool left = false;
-		bool right = false;
-		bool up = false;
-		bool down = false;
-	} keys;
+    struct
+    {
+        bool left = false;
+        bool right = false;
+        bool up = false;
+        bool down = false;
+    } keys;
 
-	bool moving();
-	float getNearClip();
-	float getFarClip();
-	void setPerspective(float fov, float aspect, float znear, float zfar);
-	void updateAspectRatio(float aspect);
-	void setPosition(glm::vec3 position);
-	void setRotation(glm::vec3 rotation);
-	void rotate(glm::vec3 delta);
-	void setTranslation(glm::vec3 translation);
-	void translate(glm::vec3 delta);
-	void update(float deltaTime);
-	bool updatePad(glm::vec2 axisLeft, glm::vec2 axisRight, float deltaTime);
+    bool moving();
+    float getNearClip();
+    float getFarClip();
+    void setPerspective(float fov, float aspect, float znear, float zfar);
+    void updateAspectRatio(float aspect);
+    void setPosition(glm::float3 position);
+    void setRotation(glm::float3 rotation);
+    void rotate(glm::float3 delta);
+    void setTranslation(glm::float3 translation);
+    void translate(glm::float3 delta);
+    void update(float deltaTime);
+    bool updatePad(glm::float2 axisLeft, glm::float2 axisRight, float deltaTime);
 };
