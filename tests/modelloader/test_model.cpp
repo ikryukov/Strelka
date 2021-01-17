964a5e0cddc263f5eac649ff8b28705263637d11
jswark <swark.a.j@gmail.com>
2021-01-17 16:16:40 +0300

    fix tests

--- tests/modelloader/test_model.cpp
+++ tests/modelloader/test_model.cpp
@@ -11,15 +11,7 @@ TEST_CASE("load model")
     nevk::Model model;
     bool loaded = model.loadModel(MODEL_PATH, MTL_PATH, mScene);
 
-    CHECK(loaded == true);
-}
-
-TEST_CASE("check model data")
-{
-    nevk::Scene mScene;
-    nevk::Model model;
-    model.loadModel(MODEL_PATH, MTL_PATH, mScene);
-
     CHECK(model.getIndices().size() == 36);
     CHECK(model.getVertices().size() == 36);
+    CHECK(loaded == true);
 }
