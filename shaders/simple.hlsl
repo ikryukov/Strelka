01d9783d7fd21889c2c652acfc580a6929e1a99b
Polina Ozhiganova <irkaxortiza@mail.ru>
2021-03-12 23:26:01 +0300

    app phong model to shader

--- shaders/simple.hlsl
+++ shaders/simple.hlsl
@@ -28,6 +28,7 @@ struct PS_INPUT
     float3 normal;
     float2 uv;
     nointerpolation uint32_t materialId;
+    float3 wPos; // new
 };
 
 cbuffer ubo
@@ -35,7 +36,9 @@ cbuffer ubo
     float4x4 modelViewProj;
     float4x4 worldToView;
     float4x4 inverseWorldToView;
+    float3 CameraPos; // new
 }
+
 Texture2D tex;
 SamplerState gSampler;
 StructuredBuffer<Material> materials;
@@ -43,12 +46,13 @@ StructuredBuffer<Material> materials;
 [shader("vertex")]
 PS_INPUT vertexMain(VertexInput vi)
 {
+
     PS_INPUT out;
     out.pos = mul(modelViewProj, float4(vi.position, 1.0f));
     out.uv = vi.uv;
     out.normal = mul((float3x3)inverseWorldToView, vi.normal);
     out.materialId = vi.materialId;
-
+    out.wPos = vi.position;  // ???
     return out;
 }
 
@@ -73,5 +77,33 @@ float4 fragmentMain(PS_INPUT inp) : SV_TARGET
    uint32_t texSpeculaId = 0;
    uint32_t texNormalId = 0;
 
-   return float4(abs(inp.normal), 1.0);
+   //return float4(abs(inp.normal), 1.0);
+
+   /////// new ////////
+  // light direction
+  // float3 lightDir = normalize(inp.pos - inp.wPos);
+  float3 lightDir = float3(10.0f,10.0f,10.0f);
+  // per pixel diffuse lighting
+  //float diffuseLighting = saturate(dot(inp.normal, -lightDir));
+  float diffuseLighting = dot(inp.normal, lightDir);
+
+  //diffuseLighting *= ((length(lightDir) * length(lightDir)) / dot(light.Position - inp.wPos, light.Position - inp.wPos));
+  diffuseLighting *= ((length(lightDir) * length(lightDir)) / dot( float3(5.0f,5.0f,5.0f) - inp.wPos,  float3(5.0f,5.0f,5.0f) - inp.wPos));
+
+  float3 h = normalize(normalize(CameraPos - inp.wPos) - lightDir);
+  //float specLighting = pow(saturate(dot(h, inp.normal)), 2.0f);
+  float specLighting = pow(dot(h, inp.normal), 2.0f);
+
+  return specLighting;
+
+   // return float4(
+    //   ambient +
+    //    (diffuse  * diffuseLighting * 0.6) + // Use light diffuse vector as intensity multiplier
+     //   (specular  * specLighting * 0.5) // Use light specular vector as intensity multiplier
+      //  , 1);
+
 }
+
+
+
+
