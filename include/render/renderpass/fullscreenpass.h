57489dd157dc21d6fae0dbc6f6234142828ea203
ArinaMonicheva <63364781+ArinaMonicheva@users.noreply.github.com>
2021-03-28 19:26:04 +0300

    fullscreen.h

--- include/render/renderpass/fullscreenpass.h
+++ include/render/renderpass/fullscreenpass.h
@@ -1,20 +1,43 @@
 #pragma once
-
-#include "renderpass/renderpass.h"
+#include <vulkan/vulkan.h>
+#include "vertex.h"
+#include <vector>
+#include <array>
+#include <resourcemanager.h>
+#include <glm/gtx/compatibility.hpp>
+#include "renderpass.h"
 
 namespace nevk
 {
-
-class FullScreenPass : public RenderPass
+class FullscreenPass : public RenderPass
 {
-protected:
-    void createGraphicsPipeline() override;
-
-    void createFrameBuffers(std::vector<VkImageView>& imageViews);
+private:
+    VkImageView mTextureImageView;
+    VkImageView mOutImageView;
+    VkSampler mTextureSampler;
 
 public:
-    void record(VkCommandBuffer& cmd, uint32_t imageIndex);
-    void onResize(std::vector<VkImageView>& imageViews, uint32_t width, uint32_t height);
+
+    void init(VkDevice& device, const char* vsCode, uint32_t vsCodeSize, const char* psCode, uint32_t psCodeSize, VkDescriptorPool descpool, ResourceManager* resMngr, uint32_t width, uint32_t height);
+
+    void record(VkCommandBuffer& cmd, uint32_t indicesCount, uint32_t width, uint32_t height, uint32_t imageIndex);
+
+    void createFrameBuffers(std::vector<VkImageView>& imageViews, VkImageView& depthImageView, uint32_t width, uint32_t height);
+
+    void onResize(std::vector<VkImageView>& imageViews, VkImageView& depthImageView, uint32_t width, uint32_t height);
+
+    void createDescriptorSets(VkDescriptorPool& descriptorPool);
+
+    void updateDescriptorSets();
+
+    void setOutputImageView(VkImageView imageView)
+    {
+        mOutImageView = imageView;
+    }
+	
+	FullscreenPass(/* args */);
+    ~FullscreenPass();
+
 };
 
 } // namespace nevk
