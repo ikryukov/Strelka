161678f12fbc686935531ea6986e8665af26af8b
Ilya Kryukov <ilyakrukov@gmail.com>
2021-09-18 21:09:22 +0300

    polish shader code

--- shaders/accumulation.hlsl
+++ shaders/accumulation.hlsl
@@ -6,24 +6,16 @@ Texture2D<float> currTex;
 Texture2D<float> prevTex;
 RWTexture2D<float> output;
 
-
 float acc(uint2 pixelIndex)
 {
     float current = currTex[pixelIndex];
+    // pixel coord -> ndc
+    float2 currNdc = (2.0 * pixelIndex) / ubo.dimension - 1.0;
     float2 mv = motion[pixelIndex];
-
-    float2 currNdc;
-    currNdc.x = (2.0 * pixelIndex.x) / ubo.dimension.x - 1.0;
-    currNdc.y = (2.0 * pixelIndex.y) / ubo.dimension.y - 1.0;
-
-    currNdc += mv;
-
-    uint2 prevPixel;
-    prevPixel.x = ubo.dimension.x * 0.5 * currNdc.x + ubo.dimension.x * 0.5;
-    prevPixel.y = ubo.dimension.y * 0.5 * currNdc.y + ubo.dimension.y * 0.5;
-
+    currNdc += mv; // moved to prev ndc
+    // prev pixel screen coord
+    uint2 prevPixel = ubo.dimension * 0.5 * currNdc + ubo.dimension * 0.5;
     float prev = prevTex[pixelIndex];
-
     return lerp(prev, current, ubo.alpha);
 }
 
