5cff96daff9c40e8a2648fce4d7cb448ff6ee3fb
jswark <swark.a.j@gmail.com>
2021-10-13 21:46:32 +0300

    fix win

--- scripts/miscLoader.sh
+++ scripts/miscLoader.sh
@@ -3,15 +3,15 @@
 cd ../misc/
 
 # download vespa
-wget --content-disposition "https://drive.google.com/uc?id=1hfs3smqD-cpFEOMj1lbT3ASkW6cF0KNu"
+wget "https://drive.google.com/uc?id=1hfs3smqD-cpFEOMj1lbT3ASkW6cF0KNu" --no-check-certificate -O vespa.zip
 unzip vespa.zip
 
 #download pica_pica
-wget --content-disposition "https://drive.google.com/uc?id=1TkRQrpNmAjy_Vcp1AQNSTORc_52MEVO4"
+wget "https://drive.google.com/uc?id=1TkRQrpNmAjy_Vcp1AQNSTORc_52MEVO4" --no-check-certificate -O pica_pica.zip
 unzip pica_pica.zip
 
 #download bathroom
-wget --content-disposition "https://drive.google.com/uc?id=1e89qGb0nNrujmei50H_U8XfTq4Rie817"
+wget "https://drive.google.com/uc?id=1e89qGb0nNrujmei50H_U8XfTq4Rie817" --no-check-certificate -O bathroom.zip
 unzip bathroom.zip
 
 # delete .zip
