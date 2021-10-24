bff703656d1c62d9d555b975260182585132918e
Ilya Kryukov <ilyakrukov@gmail.com>
2021-10-24 17:57:21 +0300

    some fixes in shader

--- shaders/pathtracer.hlsl
+++ shaders/pathtracer.hlsl
@@ -11,7 +11,7 @@ ConstantBuffer<PathTracerParam> ubo;
 Texture2D<float4> gbWPos;
 Texture2D<float4> gbNormal;
 Texture2D<int> gbInstId;
-Texture2D<float4> gbUV;
+Texture2D<float2> gbUV;
 
 StructuredBuffer<BVHNode> bvhNodes;
 StructuredBuffer<Vertex> vb;
@@ -25,9 +25,17 @@ SamplerState samplers[BINDLESS_SAMPLER_COUNT];
 
 RWTexture2D<float4> output;
 
-float3 calcReflection(uint2 pixelIndex)
+float3 UniformSampleRect(RectLight l, float2 u)
+{
+    float3 e1 = l.points[1].xyz - l.points[0].xyz;
+    float3 e2 = l.points[3].xyz - l.points[0].xyz;
+    return l.points[0].xyz + e1 * u.x + e2 * u.y;
+}
+
+float3 pathTrace(uint2 pixelIndex)
 {
     float4 gbWorldPos = gbWPos[pixelIndex];
+    // early out - miss on camera ray
     if (gbWorldPos.w == 0.0)
         return 0;
     float3 wpos = gbWPos[pixelIndex].xyz;
@@ -55,14 +63,33 @@ float3 calcReflection(uint2 pixelIndex)
     accel.vb = vb;
     accel.ib = ib;
 
+    // calc camera ray
+    RectLight currLight = lights[0]; // TODO: sample lights
+    const float3 pointOnLight = UniformSampleRect(currLight, float2(rand(rngState), rand(rngState)));
+    float3 L = normalize(pointOnLight - ray.o.xyz);
+    Ray shadowRay;
+    shadowRay.d = float4(L, 0.0);
+    shadowRay.o = float4(ray.o.xyz, distance(pointOnLight, ray.o.xyz));
+    Hit shadowHit;
+    shadowHit.t = 0.0;
+    float shadow = anyHit(accel, shadowRay, shadowHit) ? 0.0 : 1.0;
+    float2 matUV = gbUV[pixelIndex];
+    InstanceConstants instConst = accel.instanceConstants[gbInstId[pixelIndex]];
+    Material material = materials[instConst.materialId];    float3 finalColor = shadow * dot(N, L) * getBaseColor(material, matUV, textures, samplers);
+
     int depth = 0;
     int maxDepth = ubo.maxDepth;
-    float3 finalColor = float3(0.f, 0.f, 0.f);
     while (depth < maxDepth)
     {
+        // generate new ray 
+        TBN = GetTangentSpace(N); // N - hit normal
+        u1 = rand(rngState);
+        u2 = rand(rngState);
+        tangentSpaceDir = SampleHemisphere(u1, u2, 1.0); // 0 - uniform sampling, 1 - cos. sampling, higher for phong
+        dir = mul(TBN, tangentSpaceDir);
+        ray.d = float4(dir, 0.0);
         if (closestHit(accel, ray, hit))
         {
-            float2 bcoords = hit.bary;
             InstanceConstants instConst = accel.instanceConstants[hit.instId];
             Material material = materials[instConst.materialId];
 
@@ -73,9 +100,11 @@ float3 calcReflection(uint2 pixelIndex)
             float3 n0 = unpackNormal(accel.vb[i0].normal);
             float3 n1 = unpackNormal(accel.vb[i1].normal);
             float3 n2 = unpackNormal(accel.vb[i2].normal);
-
+            
+            float2 bcoords = hit.bary;
             float3 n = interpolateAttrib(n0, n1, n2, bcoords);
-
+            N = n; // hit normal
+            ray.o.xyz = ray.o.xyz + hit.t * ray.d.xyz; // new ray origin for next ray
             float2 uv0 = unpackUV(accel.vb[i0].uv);
             float2 uv1 = unpackUV(accel.vb[i1].uv);
             float2 uv2 = unpackUV(accel.vb[i2].uv);
@@ -91,14 +120,7 @@ float3 calcReflection(uint2 pixelIndex)
 
             finalColor += dcol;
 
-            // set new dir
-            ray.o = ray.d;
-            TBN = GetTangentSpace(n);
-            u1 = rand(rngState);
-            u2 = rand(rngState);
-            tangentSpaceDir = SampleHemisphere(u1, u2, 1.0); // 0 - uniform sampling, 1 - cos. sampling, higher for phong
-            dir = mul(TBN, tangentSpaceDir);
-            ray.d = float4(dir, 0.0);
+
 
             depth += 1;
         }
@@ -123,7 +145,7 @@ void computeMain(uint2 pixelIndex : SV_DispatchThreadID)
 
     float3 color = 0.f;
 
-    color = calcReflection(pixelIndex);
+    color = pathTrace(pixelIndex);
 
     output[pixelIndex] = float4(color, 1.0);
 }
