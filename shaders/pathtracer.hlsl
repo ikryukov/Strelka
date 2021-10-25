a4ecc9fbdbd62017744a5dbce2161c1f586841e0
jswark <swark.a.j@gmail.com>
2021-10-25 20:55:07 +0300

    fake fix

--- shaders/pathtracer.hlsl
+++ shaders/pathtracer.hlsl
@@ -67,65 +67,85 @@ float3 pathTrace(uint2 pixelIndex)
     RectLight currLight = lights[0]; // TODO: sample lights
     const float3 pointOnLight = UniformSampleRect(currLight, float2(rand(rngState), rand(rngState)));
     float3 L = normalize(pointOnLight - ray.o.xyz);
+
     Ray shadowRay;
     shadowRay.d = float4(L, 0.0);
     shadowRay.o = float4(ray.o.xyz, distance(pointOnLight, ray.o.xyz));
+
     Hit shadowHit;
     shadowHit.t = 0.0;
     float shadow = anyHit(accel, shadowRay, shadowHit) ? 0.0 : 1.0;
+
     float2 matUV = gbUV[pixelIndex];
     InstanceConstants instConst = accel.instanceConstants[gbInstId[pixelIndex]];
-    Material material = materials[instConst.materialId];    float3 finalColor = shadow * dot(N, L) * getBaseColor(material, matUV, textures, samplers);
+    Material material = materials[instConst.materialId];
+    float3 finalColor = shadow * dot(N, L) * getBaseColor(material, matUV, textures, samplers);
 
     int depth = 0;
     int maxDepth = ubo.maxDepth;
+    const float pdf = 1 / 2 * PI;
+
     while (depth < maxDepth)
     {
-        // generate new ray 
-        TBN = GetTangentSpace(N); // N - hit normal
-        u1 = rand(rngState);
-        u2 = rand(rngState);
-        tangentSpaceDir = SampleHemisphere(u1, u2, 1.0); // 0 - uniform sampling, 1 - cos. sampling, higher for phong
-        dir = mul(TBN, tangentSpaceDir);
-        ray.d = float4(dir, 0.0);
-        if (closestHit(accel, ray, hit))
-        {
-            InstanceConstants instConst = accel.instanceConstants[hit.instId];
-            Material material = materials[instConst.materialId];
+            // generate new ray
+            TBN = GetTangentSpace(N); // N - hit normal
+            u1 = rand(rngState);
+            u2 = rand(rngState);
+            tangentSpaceDir = SampleHemisphere(u1, u2, 1.0); // 0 - uniform sampling, 1 - cos. sampling, higher for phong
+            dir = mul(TBN, tangentSpaceDir);
+            ray.d = float4(dir, 0.0);
 
-            uint i0 = accel.ib[instConst.indexOffset + hit.primId * 3 + 0];
-            uint i1 = accel.ib[instConst.indexOffset + hit.primId * 3 + 1];
-            uint i2 = accel.ib[instConst.indexOffset + hit.primId * 3 + 2];
-
-            float3 n0 = unpackNormal(accel.vb[i0].normal);
-            float3 n1 = unpackNormal(accel.vb[i1].normal);
-            float3 n2 = unpackNormal(accel.vb[i2].normal);
-            
-            float2 bcoords = hit.bary;
-            float3 n = interpolateAttrib(n0, n1, n2, bcoords);
-            N = n; // hit normal
-            ray.o.xyz = ray.o.xyz + hit.t * ray.d.xyz; // new ray origin for next ray
-            float2 uv0 = unpackUV(accel.vb[i0].uv);
-            float2 uv1 = unpackUV(accel.vb[i1].uv);
-            float2 uv2 = unpackUV(accel.vb[i2].uv);
-
-            float2 uvCoord = interpolateAttrib(uv0, uv1, uv2, bcoords);
-
-            float3 dcol = getBaseColor(material, uvCoord, textures, samplers);
-
-            if (material.isLight)
+            if (closestHit(accel, ray, hit))
             {
-                return finalColor + dcol;
-            }
+                instConst = accel.instanceConstants[hit.instId];
+                material = materials[instConst.materialId];
 
-            finalColor += dcol;
-            depth += 1;
-        }
-        else
-        {
-            // missed
-           depth = maxDepth; // kind of return
-        }
+                uint i0 = accel.ib[instConst.indexOffset + hit.primId * 3 + 0];
+                uint i1 = accel.ib[instConst.indexOffset + hit.primId * 3 + 1];
+                uint i2 = accel.ib[instConst.indexOffset + hit.primId * 3 + 2];
+
+                float3 n0 = unpackNormal(accel.vb[i0].normal);
+                float3 n1 = unpackNormal(accel.vb[i1].normal);
+                float3 n2 = unpackNormal(accel.vb[i2].normal);
+
+                float2 bcoords = hit.bary;
+                float3 n = interpolateAttrib(n0, n1, n2, bcoords);
+                N = n; // hit normal
+
+                float2 uv0 = unpackUV(accel.vb[i0].uv);
+                float2 uv1 = unpackUV(accel.vb[i1].uv);
+                float2 uv2 = unpackUV(accel.vb[i2].uv);
+
+                float2 uvCoord = interpolateAttrib(uv0, uv1, uv2, bcoords);
+
+                float3 dcol = getBaseColor(material, uvCoord, textures, samplers);
+
+                if (material.isLight)
+                {
+                    return finalColor;
+                }
+
+                L = normalize(pointOnLight - ray.o.xyz);
+
+                shadowRay.d = float4(L, 0.0);
+                shadowRay.o = float4(ray.o.xyz, distance(pointOnLight, ray.o.xyz));
+
+                shadowHit.t = 0.0;
+                shadow = anyHit(accel, shadowRay, shadowHit) ? 0.0 : 1.0;
+
+
+                finalColor += dcol * dot(N, L); // * shadow / pdf;
+
+                ray.o.xyz = ray.o.xyz + hit.t * ray.d.xyz; // new ray origin for next ray
+                depth += 1;
+            }
+            else
+            {
+                // ray left the scene
+                finalColor *= float3(0.f);
+                depth = maxDepth; // kind of return
+            }
+        depth += 1;
     }
 
     return finalColor;
