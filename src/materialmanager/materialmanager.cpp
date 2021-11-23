5474adf11d0e8ff31c04715be0860adffe01c928
jswark <swark.a.j@gmail.com>
2021-11-23 21:37:22 +0300

    fix

--- src/materialmanager/materialmanager.cpp
+++ src/materialmanager/materialmanager.cpp
@@ -195,7 +195,7 @@ public:
 
     uint32_t getResourceInfoSize(const TargetCode* targetCode)
     {
-        return targetCode->internalsInfo.size() * sizeof(TargetCode);
+        return targetCode->internalsInfo.size() * sizeof(MdlHlslCodeGen::InternalMaterialInfo);
     }
 
     const uint8_t* getResourceInfoData(const TargetCode* targetCode)
