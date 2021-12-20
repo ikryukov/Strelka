51393003e89b86a3ec8b990818699f1a902f5113
Ilya Kryukov <ilyakrukov@gmail.com>
2021-12-21 00:44:24 +0300

    fixed mac build

--- include/render/vkrender.h
+++ include/render/vkrender.h
@@ -30,9 +30,8 @@ const std::vector<const char*> validationLayers = {
 };
 
 const std::vector<const char*> deviceExtensions = {
-    //VK_KHR_SWAPCHAIN_EXTENSION_NAME
+    //VK_KHR_SWAPCHAIN_EXTENSION_NAME,
 #ifdef __APPLE__
-    ,
     "VK_KHR_portability_subset"
 #endif
 };
