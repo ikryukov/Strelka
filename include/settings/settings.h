aa166a4608e102ed2204213fa71131f0caccd28e
Ilya Kryukov <ilyakrukov@gmail.com>
2022-03-20 23:52:34 +0400

    reformat

--- include/settings/settings.h
+++ include/settings/settings.h
@@ -1,4 +1,4 @@
-#pragma once 
+#pragma once
 
 #include <string>
 #include <unordered_map>
@@ -6,29 +6,27 @@
 namespace oka
 {
 
-    class SettingsManager
+class SettingsManager
+{
+private:
+    /* data */
+    std::unordered_map<std::string, std::string> mMap;
+
+public:
+    SettingsManager(/* args */);
+    ~SettingsManager();
+
+    template <typename T>
+    void setAs(std::string& name, T& value)
     {
-    private:
-        /* data */
-        std::unordered_map<std::string, std::string> mMap;
-    public:
-        SettingsManager(/* args */);
-        ~SettingsManager();
+        mMap[name] = std::to_string(value);
+    }
 
-        template<typename T>
-        void setAs(std::string& name, T& value)
-        {
-            mMap[name] = std::to_string(value);
-        }
-
-        template<typename T>
-        T getAs(std::string& name)
-        {
-            return mMap[name];
-        }
-
-    };
-    
-
+    template <typename T>
+    T getAs(std::string& name)
+    {
+        return mMap[name];
+    }
+};
 
 } // namespace oka
