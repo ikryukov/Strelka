8b8f4818676e354730fd06c86cb2285f717c5bfc
Ilya Kryukov <ilyakrukov@gmail.com>
2022-05-17 19:54:48 +0400

    added flag for latest sdk

--- src/render/vkrender.cpp
+++ src/render/vkrender.cpp
@@ -135,6 +135,8 @@ void VkRender::createInstance()
     createInfo.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
     createInfo.ppEnabledExtensionNames = extensions.data();
 
+    createInfo.flags = VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
+
     VkDebugUtilsMessengerCreateInfoEXT debugCreateInfo;
     if (enableValidationLayers)
     {
@@ -536,6 +538,7 @@ std::vector<const char*> VkRender::getRequiredExtensions()
         extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
     }
     extensions.push_back(VK_KHR_GET_PHYSICAL_DEVICE_PROPERTIES_2_EXTENSION_NAME);
+    extensions.push_back(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
     return extensions;
 }
 
