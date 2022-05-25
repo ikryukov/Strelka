d0633a1cc6de1cfc68dfc6dd98adf03c14131630
Ilya Kryukov <ilyakrukov@gmail.com>
2022-05-25 23:08:53 +0400

    fixed assert include

--- include/settings/settings.h
+++ include/settings/settings.h
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <string>
 #include <unordered_map>
+#include <cassert>
 
 namespace oka
 {
--- tests/materialmanager/test_materialmanager.cpp
+++ tests/materialmanager/test_materialmanager.cpp
@@ -6,6 +6,7 @@
 #include <filesystem>
 #include <fstream>
 #include <iostream>
+#include <cassert>
 
 using namespace oka;
 namespace fs = std::filesystem;
